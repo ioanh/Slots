8c4592409d1780720360d1055d330f157b93e159
Yoan Hristov <ioan.xristov@abv.bg>
2022-03-15 19:26:03 +0300

    adding constant values - max width and max height

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Button, View } from 'react-native';
+import {Constants} from './Constants.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <div style={playContainer}>asdas</div>
+      <div style={buttonContainer}>asfas</div>
     </View>
   );
 }
@@ -14,8 +16,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttonContainer: {
+    width: Constants.MAX_WIDTH,
+    height: 60,
+    color: blue,
+  },
+  playContainer:{
+    width: Constants.MAX_WIDTH,
+    height: 60,
+    color: red,
+  },
+
 });
